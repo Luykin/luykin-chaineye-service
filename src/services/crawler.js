7898923906472f0327bb9e284341f511dd2ce441
yukun luo <530366891@qq.com>
2024-11-13 07:05:05 +0000

    代码优化

--- src/services/crawler.js
+++ src/services/crawler.js
@@ -10,17 +10,17 @@ function joinUrl(path, projectName) {
 	if (String(path).includes('javascript:void(0)')) {
 		return `javascript:void(0)/${projectName || uuidv4()}`;
 	}
-	
+
 	// 如果 path 没有协议，拼接 baseRootDataURL
 	if (!/^https?:\/\//i.test(path)) {
 		const base = baseRootDataURL.replace(/\/+$/, ''); // 移除 base 末尾的多余斜杠
 		path = path.replace(/^\/+/, ''); // 移除 path 开头的多余斜杠
 		path = `${base}/${path}`;
 	}
-	
+
 	// 去除多余的斜杠，确保中间只有一个斜杠
 	path = path.replace(/([^:]\/)\/+/g, '$1');
-	
+
 	// 清理重复的 URL 参数
 	const url = new URL(path);
 	const params = new URLSearchParams();
@@ -28,24 +28,24 @@ function joinUrl(path, projectName) {
 		if (!params.has(key)) params.append(key, value);
 	});
 	url.search = params.toString();
-	
+
 	// 清理重复的锚点
 	if (url.hash) {
 		const uniqueHash = Array.from(new Set(url.hash.split('#'))).join('');
 		url.hash = uniqueHash;
 	}
-	
+
 	return url.toString();
 }
 
 function parseAmount(valueStr) {
 	if (!valueStr || valueStr === '--') return null;
-	
+
 	// 移除美元符号、空格以及"美元"字样
 	valueStr = valueStr.replace('$', '').replace('美元', '').trim();
-	
+
 	let multiplier = 1;
-	
+
 	if (valueStr.endsWith('M')) {
 		multiplier = 1e6;
 		valueStr = valueStr.replace('M', '').trim();
@@ -62,7 +62,7 @@ function parseAmount(valueStr) {
 		multiplier = 1e8;
 		valueStr = valueStr.replace('亿', '').trim();
 	}
-	
+
 	// 转换为浮点数并乘以相应的单位
 	const value = parseFloat(valueStr);
 	return isNaN(value) ? null : value * multiplier;
@@ -70,10 +70,10 @@ function parseAmount(valueStr) {
 
 function parseDate(dateStr) {
 	if (!dateStr) return null;
-	
+
 	const currentYear = new Date().getFullYear();
 	let formattedDateStr;
-	
+
 	// 英文日期格式处理
 	if (/^[A-Za-z]{3} \d{2}, \d{4}$/.test(dateStr)) {
 		formattedDateStr = dateStr;
@@ -82,7 +82,7 @@ function parseDate(dateStr) {
 	} else if (/^[A-Za-z]{3} \d{2}$/.test(dateStr)) {
 		formattedDateStr = `${dateStr}, ${currentYear}`;
 	}
-	
+
 	// 中文日期格式处理
 	else if (/^\d{4}-\d{2}-\d{2}$/.test(dateStr)) {
 		// 格式为 "2022-11-08"
@@ -91,7 +91,7 @@ function parseDate(dateStr) {
 		// 格式为 "11-08"，无年份
 		formattedDateStr = `${currentYear}-${dateStr}`;
 	}
-	
+
 	// 格式化为时间戳
 	const timestamp = Date.parse(formattedDateStr);
 	return isNaN(timestamp) ? null : timestamp;
@@ -104,7 +104,7 @@ class FundraisingCrawler {
 		this.detailPage = null; // 用于爬取列表项目的详情数据
 		this.socialPage = null; // 用于爬取详情的项目的社交媒体信息
 	}
-	
+
 	async initBrowser() {
 		if (!this.browser) {
 			console.log('初始化浏览器...');
@@ -117,7 +117,7 @@ class FundraisingCrawler {
 			});
 		}
 	}
-	
+
 	async initListPage() {
 		await this.initBrowser();
 		if (this.listPage) {
@@ -127,7 +127,7 @@ class FundraisingCrawler {
 		this.listPage = await this.browser.newPage();
 		console.log('初始化爬取列表页的浏览器选项卡...');
 	}
-	
+
 	async initDetailPage() {
 		await this.initBrowser();
 		if (this.detailPage) {
@@ -137,7 +137,7 @@ class FundraisingCrawler {
 		this.detailPage = await this.browser.newPage();
 		console.log('初始化爬取详情页的浏览器选项卡...');
 	}
-	
+
 	async initSocialPage() {
 		await this.initBrowser();
 		if (this.socialPage) {
@@ -162,7 +162,7 @@ class FundraisingCrawler {
 			console.error('Error closing browser:', err);
 		}
 	}
-	
+
 	/**
 	 * 关闭浏览器，但是有运行的情况下不会关闭
 	 * **/
@@ -201,14 +201,14 @@ class FundraisingCrawler {
 				return !!document.querySelector('tr.b-table-empty-row');
 			});
 			if (isEmpty) return []; // 如果是空页面则返回空数组
-			
+
 			// Wait for the table to load
 			await this.listPage.waitForSelector('.main_container');
-			
+
 			// Extract data
 			const fundraisingData = await this.listPage.evaluate(async () => {
 				const rows = document.querySelectorAll('.main_container tr');
-				
+
 				const data = Array.from(rows).slice(1).map(async row => {
 					const cells = row.querySelectorAll('td');
 					const projectElement = cells[0]?.querySelector('.name .list_name');
@@ -216,15 +216,15 @@ class FundraisingCrawler {
 					const projectName = projectElement?.childNodes?.[0]?.textContent?.trim();
 					const projectLink = projectElement?.getAttribute('href');
 					const projectDescription = cells[0]?.textContent?.trim().replace(projectName, '').trim();
-					
+
 					const round = cells[1]?.textContent?.trim();
-					
+
 					const amount = cells[2]?.textContent?.trim();
-					
+
 					const valuation = cells[3]?.textContent?.trim();
-					
+
 					const date = cells[4]?.textContent?.trim();
-					
+
 					return {
 						logo,
 						projectName,
@@ -237,12 +237,12 @@ class FundraisingCrawler {
 						isInitial: true,
 					};
 				});
-				
+
 				// 使用 Promise.all 等待所有行的异步操作完成
 				return await Promise.all(data);
 			});
 			// console.log(fundraisingData, 'fundraisingData');
-			
+
 			return fundraisingData.map(_ => {
 				const formattedAmount = parseAmount(_.amount);
 				const formattedValuation = parseAmount(_.valuation);
@@ -271,18 +271,18 @@ class FundraisingCrawler {
 		}
 		let currentPage = startPage;
 		let hasMoreData = true;
-		
+
 		try {
 			await this.initListPage();
 			state.status = 'running';
 			await state.save();
-			
+
 			while (hasMoreData) {
 				if (!this.listPage) {
 					throw new Error('fullCrawl外层拦截，本次遍历结束。应该开启了下一次。【网页不见了】');
 				}
 				console.log(`Crawling page ${currentPage}...`);
-				
+
 				const data = await retry(
 					async () => {
 						return await this.crawlPage(currentPage);
@@ -293,22 +293,22 @@ class FundraisingCrawler {
 						maxTimeout: 5000
 					}
 				);
-				
+
 				if (data?.length === 0) {
 					hasMoreData = false;
 					continue;
 				}
-				
+
 				// 获取所有字段，排除不需要更新的字段
 				const fieldsToUpdate = Object.keys(Fundraising.Project.rawAttributes).filter(field =>
 					!['id', 'projectLink', 'createdAt', 'updatedAt'].includes(field)
 				);
-				
+
 				// 执行 bulkCreate 时使用动态字段列表
 				await Fundraising.Project.bulkCreate(data, {
 					updateOnDuplicate: fieldsToUpdate
 				});
-				
+
 				state.otherInfo = {
 					...(state.otherInfo || {}),
 					currentPage: currentPage
@@ -319,7 +319,7 @@ class FundraisingCrawler {
 				// Add delay between requests
 				await new Promise(resolve => setTimeout(resolve, 1500));
 			}
-			
+
 			state.status = 'completed';
 			await state.save();
 		} catch (error) {
@@ -341,10 +341,10 @@ class FundraisingCrawler {
 				throw new Error('quickUpdate already in progress');
 			}
 			await this.initListPage();
-			
+
 			state.status = 'running';
 			await state.save();
-			
+
 			// Only crawl first 3 pages for quick updates
 			for (let page = 1; page <= 3; page++) {
 				if (!this.listPage) {
@@ -356,14 +356,14 @@ class FundraisingCrawler {
 				const fieldsToUpdate = Object.keys(Fundraising.Project.rawAttributes).filter(field =>
 					!['id', 'projectLink', 'createdAt', 'updatedAt'].includes(field)
 				);
-				
+
 				// 执行 bulkCreate 时使用动态字段列表
 				await Fundraising.Project.bulkCreate(data, {
 					updateOnDuplicate: fieldsToUpdate
 				});
 				await new Promise(resolve => setTimeout(resolve, 2000));
 			}
-			
+
 			state.lastUpdateTime = new Date();
 			state.status = 'completed';
 			await state.save();
@@ -374,183 +374,131 @@ class FundraisingCrawler {
 			await this.close();
 		}
 	}
-	
-	/**
-	 * 爬取「isInitial true」的列表项目的【详情页】数据
-	 * **/
+
+	// 抽象的爬虫函数
+	async crawlDetails(crawlStateType, crawlQueryOptions, pageInstance, crawlType) {
+		const state = await NewCrawlState.findOne({ where: crawlStateType }) || await NewCrawlState.create(crawlStateType);
+		if (state && state.status === 'running') {
+			throw new Error(`${crawlType} crawl already in progress`);
+		}
+
+		try {
+			console.log(`开始爬取【${crawlType}】项目详情数据`);
+
+			if (!pageInstance) {
+				throw new Error('Page instance not initialized');
+			}
+
+			// 查询项目列表
+			const projectsToCrawl = await Fundraising.Project.findAll({
+				...crawlQueryOptions  // 使用展开运算符
+			});
+			console.log(`${crawlType} - ${projectsToCrawl.length || 0} 项目待爬取`);
+
+			state.status = 'running';
+			state.error = null;
+			state.otherInfo = {
+				...(state.otherInfo || {}),
+				total: projectsToCrawl.length
+			};
+			await state.save();
+
+			let remainingCount = projectsToCrawl.length;
+			let failedCount = 0;
+
+			// 遍历项目进行抓取
+			for (const project of projectsToCrawl) {
+				console.log(`【${crawlType}】开始爬取 ${project.projectName} - ${project.projectLink} 的详情信息...`);
+				try {
+					await retry(
+						async () => {
+							return await this.scrapeAndUpdateProjectDetails(project, pageInstance);
+						},
+						{
+							retries: 3,
+							minTimeout: 2000,
+							maxTimeout: 5000
+						}
+					);
+				} catch (err) {
+					console.log(`${crawlType} - ${err}`);
+					console.log(`【${crawlType}】${project.projectName} - ${project.projectLink}, 详情抓取失败了!! 继续下一个`);
+					failedCount++;
+					state.otherInfo = {
+						...(state.otherInfo || {}),
+						failed: failedCount
+					};
+				}
+
+				remainingCount--;
+				state.lastUpdateTime = new Date();
+				state.otherInfo = {
+					...(state.otherInfo || {}),
+					remaining: remainingCount,
+					projectLink: project.projectLink
+				};
+				await state.save();
+				await new Promise(resolve => setTimeout(resolve, 1000)); // 设置间隔
+			}
+
+			// 完成爬取
+			state.status = 'completed';
+			await state.save();
+
+		} catch (error) {
+			state.status = 'failed';
+			state.error = error.message;
+			await state.save();
+			throw error;
+		} finally {
+			await this.close();
+		}
+	}
+
+	// 爬取「isInitial true」的项目
 	async detailsCrawl() {
-		const state = await NewCrawlState.findOne({ where: C_STATE_TYPE.detail }) || await NewCrawlState.create(C_STATE_TYPE.detail);
-		if (state && state.status === 'running') {
-			throw new Error('Detail crawl already in progress');
-		}
-		
-		try {
-			console.log('开始爬取「isInitial true」的列表项目的【详情页】数据');
-			await this.initDetailPage();
-			
-			// 查询第一类项目：isInitial 为 true 且没有 investmentsReceived，失败次数小于等于 5
-			const initialProjectsToCrawl = await Fundraising.Project.findAll({
-				where: {
-					isInitial: true,
-					'$investmentsReceived.id$': null,
-					detailFailuresNumber: { [Op.lte]: 5 }
-				},
-				include: [
-					{
-						model: Fundraising.InvestmentRelationships,
-						as: 'investmentsReceived',
-						required: false,
-						attributes: ['id']
-					}
+		const crawlQueryOptions = {
+			where: {
+				isInitial: true,
+				'$investmentsReceived.id$': null,
+				detailFailuresNumber: { [Op.lte]: 5 }
+			},
+			include: [
+				{
+					model: Fundraising.InvestmentRelationships,
+					as: 'investmentsReceived',
+					required: false,
+					attributes: ['id']
+				}
+			],
+			order: [
+				[
+					literal('CASE WHEN "originalPageNumber" IS NULL THEN 1 ELSE 0 END'),
+					'ASC'
 				],
-				order: [
-					// 按照 originalPageNumber 排序，NULL 值在最后
-					[
-						literal('CASE WHEN "originalPageNumber" IS NULL THEN 1 ELSE 0 END'),
-						'ASC'
-					],
-					['originalPageNumber', 'ASC']
-				]
-			});
-			console.log(initialProjectsToCrawl?.length || 0, '开始爬取这些项目的详情信息');
-			state.status = 'running';
-			state.error = null;
-			state.otherInfo = {
-				...(state.otherInfo || {}),
-				total: initialProjectsToCrawl?.length
-			};
-			await state.save();
-			let remainingCount = initialProjectsToCrawl?.length || 0;
-			let failedCount = 0;
-			for (const project of initialProjectsToCrawl) {
-				if (!this.detailPage) {
-					throw new Error('detailsCrawl外层拦截，本次遍历结束。应该开启了下一次。【网页不见了，Detail page not initialized】');
-				}
-				console.log(`开始爬取 ${project.projectName} - ${project.projectLink} 的详情信息...`);
-				// 爬取项目详情逻辑
-				try {
-					await retry(
-						async () => {
-							return await this.scrapeAndUpdateProjectDetails(project, this.detailPage);
-						},
-						{
-							retries: 3,
-							minTimeout: 2000,
-							maxTimeout: 5000
-						}
-					);
-				} catch (err) {
-					console.log(err);
-					console.log(`${project.projectName} - ${project.projectLink}, 详情抓取失败了!! 继续下一个`);
-					failedCount++;
-					state.otherInfo = {
-						...(state.otherInfo || {}),
-						failed: failedCount
-					};
-				}
-				// 更新计数器并赋值给 numberDetailsToCrawl
-				remainingCount--;
-				state.lastUpdateTime = new Date();
-				state.otherInfo = {
-					...(state.otherInfo || {}),
-					remaining: remainingCount,
-					projectLink: project.projectLink
-				};
-				await state.save();
-				await new Promise(resolve => setTimeout(resolve, 1000));
-			}
-			
-			// 完成详情页爬取
-			state.status = 'completed';
-			await state.save();
-			
-		} catch (error) {
-			state.status = 'failed';
-			state.error = error.message;
-			await state.save();
-			throw error;
-		} finally {
-			await this.close();
-		}
+				['originalPageNumber', 'ASC']
+			]
+		};
+
+		// 传递具体的页面实例以及爬虫类型
+		await this.crawlDetails(C_STATE_TYPE.detail, crawlQueryOptions, this.detailPage, 'detailsCrawl');
 	}
-	
-	/**
-	 * 爬取「isInitial false」的子项目的【详情页】数据
-	 * **/
+
+	// 爬取「isInitial false」的项目
 	async subDetailsCrawl() {
-		const state = await NewCrawlState.findOne({ where: C_STATE_TYPE.detail2 }) || await NewCrawlState.create(C_STATE_TYPE.detail2);
-		if (state && state.status === 'running') {
-			throw new Error('subDetailsCrawl crawl already in progress');
-		}
-		try {
-			console.log('开始爬取【sub】项目详情页数据=====')
-			await this.initSocialPage();
-			
-			// 查询第二类项目：isInitial 为 false，失败次数小于等于 3，socialLinks 为空
-			const nonInitialProjectsToCrawl = await Fundraising.Project.findAll({
-				where: {
-					isInitial: false,
-					detailFailuresNumber: { [Op.lte]: 3 },
-					socialLinks: null
-				}
-			});
-			console.log(nonInitialProjectsToCrawl?.length || 0, '开始爬取这些【sub】项目的详情信息');
-			state.status = 'running';
-			state.error = null;
-			state.otherInfo = {
-				...(state.otherInfo || {}),
-				total: nonInitialProjectsToCrawl?.length
-			};
-			await state.save();
-			let remainingCount = nonInitialProjectsToCrawl?.length || 0;
-			let failedCount = 0;
-			for (const project of nonInitialProjectsToCrawl) {
-				if (!this.socialPage) {
-					throw new Error('subDetailsCrawl外层拦截，本次遍历结束。应该开启了下一次。【网页不见了，Detail page not initialized】');
-				}
-				console.log(`开始爬取【sub】 ${project.projectName} - ${project.projectLink} 的详情信息...`);
-				try {
-					await retry(
-						async () => {
-							return await this.scrapeAndUpdateProjectDetails(project, this.socialPage);
-						},
-						{
-							retries: 3,
-							minTimeout: 2000,
-							maxTimeout: 5000
-						}
-					);
-				} catch (err) {
-					console.log(err);
-					console.log(`【sub】${project.projectName} - ${project.projectLink}, 详情抓取失败了!! 继续下一个`);
-					failedCount++;
-					state.otherInfo = {
-						...(state.otherInfo || {}),
-						failed: failedCount
-					};
-				}
-				remainingCount--;
-				state.lastUpdateTime = new Date();
-				state.otherInfo = {
-					...(state.otherInfo || {}),
-					remaining: remainingCount,
-					projectLink: project.projectLink
-				};
-				await state.save();
-				await new Promise(resolve => setTimeout(resolve, 1000));
+		const crawlQueryOptions = {
+			where: {
+				isInitial: false,
+				detailFailuresNumber: { [Op.lte]: 3 },
+				socialLinks: null
 			}
-			state.status = 'completed';
-			await state.save();
-		} catch (error) {
-			state.status = 'failed';
-			state.error = error.message;
-			await state.save();
-			throw error;
-		} finally {
-			await this.close();
-		}
+		};
+
+		// 传递具体的页面实例以及爬虫类型
+		await this.crawlDetails(C_STATE_TYPE.detail2, crawlQueryOptions, this.socialPage, 'subDetailsCrawl');
 	}
-	
+
+
 	/**
 	 * 传入一个项目数据库实例，和浏览器网页，开始爬取详情
 	 * 会根据isInitial判断要不要继续深度爬取融资信息
@@ -564,7 +512,7 @@ class FundraisingCrawler {
 			// // 随机选择一个 User-Agent
 			// const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
 			// await _page.setUserAgent(randomUserAgent); // 设置 User-Agent
-			
+
 			await _page.goto(project.projectLink, {
 				waitUntil: 'networkidle0',
 				timeout: 10000
@@ -573,14 +521,14 @@ class FundraisingCrawler {
 			await _page.waitForSelector('.base_info', {
 				timeout: 10000
 			});
-			
+
 			await this.clickAllButtons(_page);
 			console.log('开始抓取这个项目更详细的详细', project.projectLink);
 			let relatedProjectLength = 0;
 			if (project.isInitial) {
 				relatedProjectLength = await this.processRounds(project, _page);
 			}
-			
+
 			// Fetch additional data
 			const details = await _page.evaluate(() => {
 				// Extract social links
@@ -591,7 +539,7 @@ class FundraisingCrawler {
 						socialLinks[type] = link.href;
 					}
 				});
-				
+
 				// Extract team members
 				const teamMembers = Array.from(document.querySelectorAll('.team_member .item')).map(member => ({
 					name: member.querySelector('.content h2')?.textContent?.trim(),
@@ -599,10 +547,10 @@ class FundraisingCrawler {
 					avatar: member.querySelector('.logo-wraper img')?.src || '',
 					profileLink: member.querySelector('.card')?.href || ''
 				}));
-				
+
 				const logo = document.querySelector('.detail_info_head .logo')?.src || '';
 				const projectName = document.querySelector('.detail_info_head h1.name')?.textContent?.trim();
-				
+
 				return { socialLinks, teamMembers, projectName, logo };
 			});
 			const isCrawlSuccess = details.projectName && details.logo
@@ -643,11 +591,11 @@ class FundraisingCrawler {
 	async processRounds(project, _page) {
 		try {
 			const roundsData = await _page.evaluate(() => {
-				
+
 				const rows = document.querySelectorAll('.investor tr');
 				return Array.from(rows).slice(1).map(row => {
 					const cells = row.querySelectorAll('td');
-					
+
 					return {
 						round: cells[0]?.textContent?.trim(),
 						amount: cells[1]?.textContent?.trim(),
@@ -672,7 +620,7 @@ class FundraisingCrawler {
 					timestamp: parseDate(_.date)
 				};
 			});
-			
+
 			// 批量处理投资人信息
 			const investorProjectsPromises = roundsDataFormatted.flatMap((round) =>
 				round.investors.map(async (investor) => {
@@ -698,7 +646,7 @@ class FundraisingCrawler {
 					};
 				})
 			);
-			
+
 			const investorProjects = await Promise.all(investorProjectsPromises);
 			// 批量创建 InvestmentRelationships 记录
 			await Fundraising.InvestmentRelationships.bulkCreate(investorProjects);
