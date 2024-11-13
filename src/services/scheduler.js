0df0f66a4271197068a4f4cd8a5782db4e54c5b7
luykin <luo530366891@gmail.com>
2024-11-13 12:50:06 +0800

    修复项目问题

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -1,6 +1,7 @@
 const schedule = require('node-schedule');
 const crawler = require('./crawler');
 const { NewCrawlState, C_STATE_TYPE } = require('../models');
+const { Op } = require('sequelize');
 
 class CrawlerScheduler {
 	constructor() {
@@ -8,26 +9,55 @@ class CrawlerScheduler {
 		this.halfHourlyDetailJob = null;
 	}
 	
-	async restartDetailCrawl() {
-		await crawler.forceClose();
+	async restartDetailCrawl(scheduledTask = false) {
+		let needRestartDetails = false;
+		let needRestartDetails2 = false;
 		/**
 		 * 开始detail crawl**/
 		const state1 = await NewCrawlState.findOne({
-			where: C_STATE_TYPE.detail
+			where: {
+				...C_STATE_TYPE.detail,
+				...(scheduledTask ? {
+					/**
+					 * 如果是定时任务，查找不等于running的
+					 * 是running的，不处理
+					 * **/
+					status: { [Op.ne]: 'running' }
+				} : {})
+			},
 		});
 		
 		if (state1) {
 			await state1.update({ status: 'idle', error: null });
-			crawler.detailsCrawl();
+			needRestartDetails = true;
 		}
 		/**
 		 * 开始sub details crawl**/
 		const state2 = await NewCrawlState.findOne({
-			where: C_STATE_TYPE.detail2
+			where: {
+				...C_STATE_TYPE.detail2,
+				...(scheduledTask ? {
+					/**
+					 * 如果是定时任务，查找不等于running的
+					 * 是running的，不处理
+					 * **/
+					status: { [Op.ne]: 'running' }
+				} : {})
+			}
 		});
 		if (state2) {
 			await state2.update({ status: 'idle', error: null });
-			crawler.subDetailsCrawl();
+			needRestartDetails2 = true;
+		}
+		if (needRestartDetails || needRestartDetails2) {
+			await new Promise(resolve => setTimeout(resolve, 2000));
+			console.log('发现需要重启浏览器,重启任务', needRestartDetails, needRestartDetails2)
+			await crawler.forceClose();
+			console.log('等待浏览器完全关闭，上一次的任务结束, 等60s');
+			await new Promise(resolve => setTimeout(resolve, 60000));
+			console.log('60s等待完毕，开始重新执行!!!!');
+			needRestartDetails && crawler.detailsCrawl();
+			needRestartDetails2 && crawler.subDetailsCrawl();
 		}
 	}
 	
@@ -45,7 +75,7 @@ class CrawlerScheduler {
 		this.halfHourlyDetailJob = schedule.scheduleJob('*/30 * * * *', async () => {
 			console.log('Starting half-hourly restartDetailCrawl...');
 			try {
-				await this.restartDetailCrawl();
+				await this.restartDetailCrawl(true);
 				console.log('Half-hourly restartDetailCrawl completed');
 			} catch (error) {
 				console.error('Half-hourly restartDetailCrawl failed:', error);
