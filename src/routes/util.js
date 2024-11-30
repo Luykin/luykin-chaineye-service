22d2b738a49805441388fb588f6b052ea80dbd0d
luykin <luo530366891@gmail.com>
2024-11-30 23:54:05 +0800

    update

--- src/routes/util.js
+++ src/routes/util.js
@@ -1,22 +1,37 @@
 const crypto = require("crypto");
 const fs = require("fs");
 const path = require("path");
+const forge = require("node-forge");
 
 const PRIVATE_KEY_PATH = path.join(__dirname, "../../private_key.pem");
 const PRIVATE_KEY = fs.readFileSync(PRIVATE_KEY_PATH, "utf8");
 
-function decryptDataWithPrivateKey(encryptedData) {
-  const buffer = Buffer.from(encryptedData, "base64");
-  const decryptedData = crypto.privateDecrypt(
-    {
-      key: PRIVATE_KEY,
-      padding: crypto.constants.RSA_PKCS1_PADDING,
-      oaepHash: "sha256",
-    },
-    buffer
-  );
-
-  return JSON.parse(decryptedData.toString("utf8"));
+/**
+ * 使用 `node-forge` 实现 PKCS#1 v1.5 填充的解密
+ * @param encryptedData 前端加密的数据 (Base64 格式)
+ * @param privateKey PEM 格式的 RSA 私钥
+ * @returns 解密后的原始数据
+ */
+function decryptDataWithPrivateKey(encryptedData, privateKey) {
+	try {
+		console.log("开始解密数据...");
+		
+		// 解码 Base64 加密数据
+		const encryptedBuffer = forge.util.decode64(encryptedData);
+		
+		// 将 PEM 私钥转换为 `forge` 支持的私钥对象
+		const forgePrivateKey = forge.pki.privateKeyFromPem(privateKey);
+		
+		// 解密数据
+		const decryptedData = forgePrivateKey.decrypt(encryptedBuffer, "RSAES-PKCS1-V1_5");
+		console.log("解密后的数据:", decryptedData);
+		
+		// 返回 JSON 格式的原始数据
+		return JSON.parse(decryptedData);
+	} catch (err) {
+		console.error("解密失败:", err);
+		throw new Error("解密失败，请检查加密数据或私钥");
+	}
 }
 
 function validateRequestParams(req, res, next) {
