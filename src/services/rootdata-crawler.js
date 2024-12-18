e382cdc4685dbfe37382b2c03ecfd0ba49be3d9c
luykin <luo530366891@gmail.com>
2024-12-19 00:11:43 +0800

    update

--- src/services/rootdata-crawler.js
+++ src/services/rootdata-crawler.js
@@ -182,29 +182,29 @@ class FundraisingCrawler extends BaseCrawler {
 			state.error = null;
 			await state.save();
 			// Only crawl first 3 pages for quick updates
-			for (let page = 1; page <= 2; page++) {
+			for (let page = 1; page <= 1; page++) {
 				if (!pageInstance || pageInstance?.isClosed?.()) {
 					throw new Error('quickUpdate: Page instance not initialized');
 				}
 				const data = await this.crawlPage(page, pageInstance);
-				// const existingLinks = await Fundraising.Project.findAll({
-				// 	attributes: ['projectLink'],
-				// 	where: {
-				// 		projectLink: data.map(item => item.projectLink)
-				// 	}
-				// }).then(projects => projects.map(project => project.projectLink));
-				// const newData = data.filter(item => !existingLinks.includes(item.projectLink));
+				const existingLinks = await Fundraising.Project.findAll({
+					attributes: ['projectLink'],
+					where: {
+						projectLink: data.map(item => item.projectLink)
+					}
+				}).then(projects => projects.map(project => project.projectLink));
+				const newData = data.filter(item => !existingLinks.includes(item.projectLink));
 				
-				if (data.length > 0) {
+				if (newData.length > 0) {
 					// 获取所有字段，排除不需要更新的字段
 					const fieldsToUpdate = Object.keys(Fundraising.Project.rawAttributes).filter(field =>
 						!['id', 'projectLink', 'createdAt', 'updatedAt'].includes(field)
 					);
 					// 执行 bulkCreate 时使用动态字段列表
-					await Fundraising.Project.bulkCreate(data, {
+					await Fundraising.Project.bulkCreate(newData, {
 						updateOnDuplicate: fieldsToUpdate
 					});
-					updateNum = updateNum + data.length;
+					updateNum = updateNum + newData.length;
 				} else {
 					console.log('No new data found on page', page);
 				}
