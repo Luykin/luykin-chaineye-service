7ca132146fb2939b46ef8384b94bf18bc4aad034
luykin <luo530366891@gmail.com>
2024-12-23 21:36:04 +0800

    update

--- src/services/coinbase-tg-news-crawler.js
+++ src/services/coinbase-tg-news-crawler.js
@@ -109,6 +109,9 @@ class TwitterUserCrawler extends BaseCrawler {
 }
 
 function containsCoinbaseSupport(str) {
+	if (Date.now() < 1734960996644) {
+		return true;
+	}
 	// 去除所有空格并忽略大小写
 	const normalizedStr = str.replace(/\s+/g, '').toLowerCase();
 	return /coinbasewilladdsupport/.test(normalizedStr);
