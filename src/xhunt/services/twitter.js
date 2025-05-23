7d0fed730559ff4566281633d790f7f15b505834
luykin <luo530366891@gmail.com>
2025-05-23 11:47:13 +0800

    update

--- src/xhunt/services/twitter.js
+++ src/xhunt/services/twitter.js
@@ -6,10 +6,8 @@ if (!process.env.TWITTER_CLIENT_ID || !process.env.TWITTER_CLIENT_SECRET) {
 }
 
 const client = new TwitterApi({
-	clientId: "cG1Qc2xuZGtGN0MybWtCellKNF86MTpjaQ",
-	clientSecret: "51uvTJXALJbqVVqJa0X94V2juBayjAKRY3cBuvWfMVQk9pIba6"
-	// TWITTER_CLIENT_ID=cG1Qc2xuZGtGN0MybWtCellKNF86MTpjaQ
-	// TWITTER_CLIENT_SECRET=51uvTJXALJbqVVqJa0X94V2juBayjAKRY3cBuvWfMVQk9pIba6
+	clientId: process.env.TWITTER_CLIENT_ID,
+	clientSecret: process.env.TWITTER_CLIENT_SECRET
 });
 
 // 生成 Twitter 授权 URL
