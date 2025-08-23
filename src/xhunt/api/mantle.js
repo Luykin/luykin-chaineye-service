8e5b1e51cc18d15b3a28ee5740cc048499e27bb5
luykin <luo530366891@gmail.com>
2025-08-23 16:30:17 +0800

    feat: Add additional special allowed usernames for invite code functionality

--- src/xhunt/api/mantle.js
+++ src/xhunt/api/mantle.js
@@ -40,6 +40,8 @@ async function ensureUniqueInviteCode() {
 const SPECIAL_INVITE_CODE = "XHuntAI";
 const SPECIAL_ALLOWED_USERNAMES = new Set([
   "luoyukun4",
+  "alpha_gege",
+  "defiteddy2020",
   "maid_crypto",
   "paris13jeanne",
   "momochenming",
