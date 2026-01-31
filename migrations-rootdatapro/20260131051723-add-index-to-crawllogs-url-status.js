4cdee5104b02e997c88feba47f60700969112235
luykin <luo530366891@gmail.com>
2026-01-31 13:27:11 +0800

    Add rootdatapro migration commands to package.json and remove obsolete CrawlLog index migration

--- migrations-rootdatapro/20260131051723-add-index-to-crawllogs-url-status.js
+++ migrations-rootdatapro/20260131051723-add-index-to-crawllogs-url-status.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addIndex('RootdataCrawlLogs', ['url', 'status'], {
+    await queryInterface.addIndex('CrawlLogs', ['url', 'status'], {
       name: 'url_status_idx',
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeIndex('RootdataCrawlLogs', 'url_status_idx');
+    await queryInterface.removeIndex('CrawlLogs', 'url_status_idx');
   }
 };
