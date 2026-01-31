392e980cb02f690db0ca552e9c275acc67a0a8fa
luykin <luo530366891@gmail.com>
2026-01-31 13:28:44 +0800

    Update migration to reflect table name change from CrawlLogs to RootdataCrawlLogs for index on URL and status

--- migrations-rootdatapro/20260131051723-add-index-to-crawllogs-url-status.js
+++ migrations-rootdatapro/20260131051723-add-index-to-crawllogs-url-status.js
@@ -3,12 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addIndex('CrawlLogs', ['url', 'status'], {
+    await queryInterface.addIndex('RootdataCrawlLogs', ['url', 'status'], {
       name: 'url_status_idx',
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeIndex('CrawlLogs', 'url_status_idx');
+    await queryInterface.removeIndex('RootdataCrawlLogs', 'url_status_idx');
   }
 };
