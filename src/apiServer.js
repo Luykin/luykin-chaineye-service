aeabeccc2ee92141bd5ac6c1f0602ff244da92ed
luykin <luo530366891@gmail.com>
2026-07-09 23:21:16 +0800

    feature: update

--- src/apiServer.js
+++ src/apiServer.js
@@ -388,7 +388,7 @@ async function initializeAndStartServer() {
       contentSecurityPolicy: {
         directives: {
           ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-          "script-src": ["'self'", "'unsafe-inline'"],
+          "script-src": ["'self'", "'unsafe-inline'", "'wasm-unsafe-eval'"],
           "style-src": ["'self'", "'unsafe-inline'"],
           "img-src": [
             "'self'",
